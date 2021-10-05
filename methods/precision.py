Fix precision and ablation

methods/precision.py
@@ -1,9 +1,7 @@
-import numpy as np
 import torch
+import torch.nn as nn
+import numpy as np
 from torch.utils.data import DataLoader
-
-from dataset import selected_loader
-from taks import calc_loss
 
 
 class Precision:
@@ -16,6 +14,8 @@ class Precision:
         k_ratio,
         precision,
         train_noise_ind,
+        device,
+        use_multi_k,
     ):
         self.criterion = criterion
         self.train_dataset = train_dataset
@@ -24,11 +24,14 @@ class Precision:
         self.k_ratio = k_ratio
         self.precision = precision
         self.train_noise_ind = train_noise_ind
+        self.device = device
+        self.use_multi_k = use_multi_k
 
-        if isinstance(self.k_ratio, list):
-            self.name = f"Precision(K_ratio-{self.k_ratio},Precision-{self.precision})"
+        if self.use_multi_k:
+            self.name = f"Precision(K_ratios-{self.k_ratio*100}%,Precision-{self.precision})"
         else:
             self.name = f"Precision(K_ratio-{self.k_ratio*100}%,Precision-{self.precision})"
+
         self.num_models = 1
         self._config()
 
@@ -40,43 +43,64 @@ class Precision:
             num_workers=16,
         )
 
-        if isinstance(self.k_ratio, list):
-            for i, k_ratio in enumerate(self.k_ratio):
-                assert 0 < k_ratio <= 1, "k_ratio should be less than 1 and greater than 0"
-                idx = np.where(np.array(self.train_dataset.coarses) == i)[0]
-                n_experts = len(idx)
-                k = int(n_experts * k_ratio)
-                player = PrecisionSelector(
-                    n_experts, k, self.precision, self.train_noise_ind
-                )
-                self.coarse_indices.append(idx)
-                self.players.append(player)
+        if self.use_multi_k:
+            k_ratios = [self.k_ratio] * len(self.train_dataset.classes)
         else:
-            assert 0 < self.k_ratio <= 1, "k_ratio should be less than 1 and greater than 0"
-            idx = list(range(len(self.train_dataset)))
+            k_ratios = [self.k_ratio]
+
+        self.class_indices = []
+        self.players = []
+        for i, k_ratio in enumerate(k_ratios):
+            if self.use_multi_k:
+                idx = np.where(self.train_dataset.targets == i)[0]
+            else:
+                idx = np.arange(len(self.train_dataset))
             n_experts = len(idx)
-            k = int(n_experts * self.k_ratio)
+            k = int(n_experts * k_ratio)
             player = PrecisionSelector(
-                n_experts, k, self.precision, self.train_noise_ind
-            )
-            self.coarse_indices.append(idx)
+                    n_experts, k, self.precision, self.train_noise_ind, self.device
+                )
+            self.class_indices.append(idx)
             self.players.append(player)
 
-    def pre_epoch_hook(self, train_dataloader, model, device):
-        loss = calc_loss(self.fixed_train_dataloader, model, device)
+    def _calc_loss(self, model):
+        # switch to evaluate mode
+        model.eval()
+
+        criterion = nn.CrossEntropyLoss(reduction="none")
+
+        losses = []
+        with torch.no_grad():
+            for i, (images, target, indexes) in enumerate(self.fixed_train_dataloader):
+                if torch.cuda.is_available():
+                    images = images.to(self.device)
+                    target = target.to(self.device)
+
+                # compute output
+                output = model(images)
+
+                # compute loss
+                loss = criterion(output, target)
+                losses.append(loss)
+
+        losses = torch.cat(losses, dim=0)
+        return losses
+
+    def pre_epoch_hook(self, model):
+        loss = self._calc_loss(model)
         
         indices = []
-        cum_losses = np.empty((len(loss)))
-        objectives = np.empty((len(loss)))
+        cum_losses = np.empty(len(loss))
+        objectives = np.empty(len(loss))
         for i, player in enumerate(self.players):
-            idx = np.where(player.w)[0]
-            coarse_idx = self.coarse_indices[i][idx]
-            cum_loss, objective = player.update(loss[coarse_idx])
-            indices = np.concatenate((indices, coarse_idx))
-            cum_losses[coarse_idx] = cum_loss
-            objectives[coarse_idx] = objective
-        selected_dataloader = selected_loader(train_dataloader, indices)
-        return loss, cum_loss, objective, [indices], selected_dataloader
+            class_idx = self.class_indices[i]
+            cum_loss, objective = player.update(loss[class_idx])
+            selected_idx = torch.where(player.w)[0].cpu()
+            indices.append(class_idx[selected_idx])
+            cum_losses[class_idx] = cum_loss.cpu()
+            objectives[class_idx] = objective.cpu()
+        indices = np.concatenate(indices)
+        return loss, cum_loss, objective, [indices]
 
     def loss(self, outputs, target, *args, **kwargs):
         output = outputs[0]
@@ -85,11 +109,12 @@ class Precision:
 
 
 class PrecisionSelector:
-    def __init__(self, n_experts, k, precision, noise_ind, fixed=True):
+    def __init__(self, n_experts, k, precision, noise_ind, device, fixed=True):
         self.n_experts = n_experts
         self.k = k
         self.precision = precision
         self.noise_ind = noise_ind
+        self.device = device
         self.fixed = fixed
 
         self.clean_ind = [i for i in range(n_experts) if i not in noise_ind]
@@ -103,8 +128,8 @@ class PrecisionSelector:
         self.w[np.random.choice(self.noise_ind, self.noise_num, replace=False)] = 1
 
     def init(self):
-        self.w = np.zeros(self.n_experts)
-        self.total_loss = torch.zeros(self.n_experts)
+        self.w = torch.zeros(self.n_experts, dtype=torch.bool, device=self.device)
+        self.total_loss = torch.zeros(self.n_experts, device=self.device)
 
         self.select()
 